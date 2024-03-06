90158937f0623d7f75bb6f51dd99ef4fa9710bef
apyd <arturpydych@gmail.com>
2024-03-06 21:46:06 +0100

    feat: add user module and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './models/users/users.controller';
+import { ArtistsController } from './models/artists/artists.controller';
+import { AlbumsController } from './models/albums/albums.controller';
+import { TracksController } from './models/tracks/tracks.controller';
+import { FavoritesController } from './models/favorites/favorites.controller';
+import { FavoritesService } from './models/favorites/favorites.service';
+import { AlbumsService } from './models/albums/albums.service';
+import { ArtistsService } from './models/artists/artists.service';
+import { UsersService } from './models/users/users.service';
+import { AlbumsModule } from './models/albums/albums.module';
+import { ArtistsModule } from './models/artists/artists.module';
+import { FavoritesModule } from './models/favorites/favorites.module';
+import { TracksModule } from './models/tracks/tracks.module';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AlbumsModule,
+    ArtistsModule,
+    FavoritesModule,
+    TracksModule,
+    UsersModule,
+  ],
+  controllers: [
+    AppController,
+    UsersController,
+    ArtistsController,
+    AlbumsController,
+    TracksController,
+    FavoritesController,
+  ],
+  providers: [FavoritesService, AlbumsService, ArtistsService, UsersService],
 })
 export class AppModule {}
